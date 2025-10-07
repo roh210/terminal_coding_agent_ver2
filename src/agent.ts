ef29fc0f94f47940160214eddbe1e88a340b0dfd
roh210 <roheena.s786@gmail.com>
2025-10-07 13:29:32 +0100

    WIP: added open ai imports , created basic cli interface
    created basic cli

---
+++ src/agent.ts
@@ -0,0 +1,52 @@
+import OpenAI from "openai";
+import * as readline from "readline/promises";
+import { Agent } from "./agent/agent.js";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+main();
+
+async function main() {
+  const client = new OpenAI({
+    baseURL: "https://openrouter.ai/api/v1",
+    apiKey: process.env.OPENROUTER_API_KEY,
+  });
+
+  const agent = new Agent(
+    client,
+    getUserMessage,
+    showAgentMessage,
+    getToolConsent
+  );
+  await agent.run();
+}
+
+async function getUserMessage(): Promise<string> {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const userMessage = await rl.question("\u001b[94mYou\u001b[0m:");
+  rl.close();
+  return userMessage;
+}
+
+function showAgentMessage(message: string): void {
+  console.log(`\n\u001b[93mLLM\u001b[0m: ${message}\n`);
+}
+
+async function getToolConsent(message: string): Promise<boolean> {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const consent = await rl.question(
+    `\n\u001b[92mTool request\u001b[0m: ${message}\n` +
+      "\u001b[93mAgent\u001b[0m: Continue? [yes]: "
+  );
+  rl.close();
+
+  return consent === "" || consent.toLowerCase() === "yes";
+}
